6ee991b6b88a321200ea9fe3c591f094963586fa
DavidPgl <panagiotopulos.d@gmail.com>
2020-10-17 23:34:38 +0200

    [Fixed include directories]

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include "Quicksort.h"
-#include "RandomizedSelect.h"
-#include "MedianOfMedians.h"
+#include "Algorithms/Quicksort.h"
+#include "Algorithms/RandomizedSelect.h"
+#include "Algorithms/MedianOfMedians.h"
 
 
 void printArray(int values[], int arraySize) {
@@ -21,11 +21,11 @@ int main()
 
 	//printArray(values, numberOfRandoms);
 
-	int* quickSortValues = new int[numberOfRandoms];
-	int* stdSortValues = new int[numberOfRandoms];
-	int* randomSelectValues = new int[numberOfRandoms];
-	int* medianMedianValues = new int[numberOfRandoms];
-	int* nthElementValues = new int[numberOfRandoms];
+	int quickSortValues[numberOfRandoms];
+	int stdSortValues[numberOfRandoms];
+	int randomSelectValues[numberOfRandoms];
+	int medianMedianValues[numberOfRandoms];
+	int nthElementValues[numberOfRandoms];
 	std::copy(std::begin(values), std::end(values), quickSortValues);
 	std::copy(std::begin(values), std::end(values), stdSortValues);
 	std::copy(std::begin(values), std::end(values), randomSelectValues);
@@ -35,25 +35,15 @@ int main()
 	Quicksort::sort(quickSortValues, 0, numberOfRandoms - 1);
 	std::cout << "Quicksort - Median: " << quickSortValues[numberOfRandoms / 2] << std::endl;
 
-	delete[] quickSortValues;
-
 	std::sort(stdSortValues, stdSortValues + numberOfRandoms);
 	std::cout << "std::sort - Median: " << stdSortValues[numberOfRandoms / 2] << std::endl;
-
-	delete[] stdSortValues;
 
 	int median = RandomizedSelect::sort(randomSelectValues, 0, numberOfRandoms - 1, (numberOfRandoms + 1) / 2);
 	std::cout << "RandomSelect - Median: " << median << std::endl;
 
-	delete[] randomSelectValues;
-
 	median = MedianOfMedians::sort(medianMedianValues, numberOfRandoms, (numberOfRandoms) / 2);
 	std::cout << "Median of medians - Median: " << median << std::endl;
 
-	delete[] medianMedianValues;
-
 	std::nth_element(nthElementValues, nthElementValues + (numberOfRandoms / 2), nthElementValues + numberOfRandoms);
 	std::cout << "STL-nth_element - Median: " << nthElementValues[numberOfRandoms / 2] << std::endl;
-
-	delete[] nthElementValues;
 }
